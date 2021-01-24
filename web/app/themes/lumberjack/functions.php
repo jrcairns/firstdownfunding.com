bdbf12de8bef1d7777861d33a2592f71f79cb40b
Justin Cairns <cairns.webdev@gmail.com>
2021-01-24 15:17:53 +0000

    initial commit

---
+++ web/app/themes/lumberjack/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Lumberjack;
+
+// Create the Application Container
+$app = require_once('bootstrap/app.php');
+
+// Bootstrap Lumberjack from the Container
+$lumberjack = $app->make(Lumberjack::class);
+$lumberjack->bootstrap();
+
+// Import our routes file
+require_once('routes.php');
+
+// Set global params in the Timber context
+add_filter('timber_context', [$lumberjack, 'addToContext']);
+
+wp_enqueue_style('styles', get_stylesheet_directory_uri(). '/style.css');
